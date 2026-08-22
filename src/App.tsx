d0a8d92bca416f72e6b301127e54f11593fb43b4
Jota <joao.bfernandes.dev@gmail.com>
2026-08-22 11:25:40 -0300

    feat: initialize Salmodia application shell

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="shell">
+      <h1>Salmodia</h1>
+      <p>Aplicativo em desenvolvimento.</p>
+    </div>
+  )
+}
+
+export default App
