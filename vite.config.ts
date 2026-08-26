07c3665ee1501211936da30602bc7850ffdb9128
Jota <joao.bfernandes.dev@gmail.com>
2026-08-26 15:19:41 -0300

    fix: update Vite base path for Cantun

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/salmodia/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/cantun/' : '/',
 
   plugins: [
     react(),
