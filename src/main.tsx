c6d7bbd765dffafbbb54443276c49a38eebe0f06
João Marcos de B. Fernandes <joao.bfernandes@sempreceub.com>
2026-08-27 15:40:37 -0300

    feat: provide auth context

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './cantum-theme.css'
 import App from './App.tsx'
+import { AuthProvider } from './auth/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
