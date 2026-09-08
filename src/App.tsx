943c9cf68457d1230ba1c64e1a8d3dc5ff6151c0
João Marcos de B. Fernandes <joao.bfernandes@sempreceub.com>
2026-09-08 00:21:03 -0300

    feat(stage): route musician readonly session page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { RepertoireListPage } from './pages/Repertoire/RepertoireListPage'
 import { RepertoireDetailPage } from './pages/Repertoire/RepertoireDetailPage'
 import { StagePage } from './pages/Stage/StagePage'
 import { BandStagePage } from './pages/Stage/BandStagePage'
+import { BandMusicianStagePage } from './pages/Stage/BandMusicianStagePage'
 import { AuthPage } from './pages/Auth/AuthPage'
 import { BandListPage, BandDetailPage, BandInvitePage } from './pages/Band/BandPage'
 import './App.css'
@@ -53,7 +54,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} /><Route path="/auth" element={<AuthPage />} />
         <Route path="/songs" element={<SongLibraryPage />} /><Route path="/repertoires" element={<RepertoireListPage />} /><Route path="/repertoires/:repertoireId" element={<RepertoireDetailPage />} />
-        <Route path="/stage/setlist/:setlistId" element={<StagePage />} /><Route path="/stage/song/:songId" element={<StagePage />} /><Route path="/stage/session/:sessionId" element={<BandStagePage />} />
+        <Route path="/stage/setlist/:setlistId" element={<StagePage />} /><Route path="/stage/song/:songId" element={<StagePage />} /><Route path="/stage/session/:sessionId" element={<BandStagePage />} /><Route path="/stage/session/:sessionId/musician" element={<BandMusicianStagePage />} />
         <Route path="/songs/new" element={<NewSongPage />} /><Route path="/songs/import" element={<ImportSongPage />} /><Route path="/songs/:songId/edit" element={<EditSongPage />} /><Route path="/songs/:songId" element={<SongDetailPage />} />
         <Route path="/bands" element={<BandListPage />} /><Route path="/bands/invite/:token" element={<BandInvitePage />} /><Route path="/bands/:bandId" element={<BandDetailPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
