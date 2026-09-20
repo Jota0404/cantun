34f5e40dcd6a08fc0b66c7de5de4c86022819889
João Marcos de B. Fernandes <joao.bfernandes@sempreceub.com>
2026-09-20 18:43:09 -0300

    feat(stage): expose target stage session route

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { RepertoireDetailPage } from './pages/Repertoire/RepertoireDetailPage'
 import { StagePage } from './pages/Stage/StagePage'
 import { BandStagePage } from './pages/Stage/BandStagePage'
 import { BandMusicianStagePage } from './pages/Stage/BandMusicianStagePage'
+import { ServiceStageSessionRedirectPage } from './pages/Stage/ServiceStageSessionRedirectPage'
 import { AuthPage } from './pages/Auth/AuthPage'
 import { BandListPage, BandDetailPage, BandInvitePage } from './pages/Band/BandPage'
 import { OrganizationPage } from './pages/Organization/OrganizationPage'
@@ -68,7 +69,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} /><Route path="/auth" element={<AuthPage />} />
         <Route path="/songs" element={<SongLibraryPage />} /><Route path="/repertoires" element={<RepertoireListPage />} /><Route path="/repertoires/:repertoireId" element={<RepertoireDetailPage />} /><Route path="/organizations/:organizationId/repertoires/:repertoireId" element={<TargetRepertoireDetailPage />} />
-        <Route path="/stage/setlist/:setlistId" element={<StagePage />} /><Route path="/stage/song/:songId" element={<StagePage />} /><Route path="/stage/session/:sessionId" element={<BandStagePage />} /><Route path="/stage/session/:sessionId/musician" element={<BandMusicianStagePage />} />
+        <Route path="/stage/setlist/:setlistId" element={<StagePage />} /><Route path="/stage/song/:songId" element={<StagePage />} /><Route path="/stage/service-session/:stageSessionId" element={<ServiceStageSessionRedirectPage />} /><Route path="/stage/session/:sessionId" element={<BandStagePage />} /><Route path="/stage/session/:sessionId/musician" element={<BandMusicianStagePage />} />
         <Route path="/songs/new" element={<NewSongPage />} /><Route path="/songs/import" element={<ImportSongPage />} /><Route path="/songs/:songId/edit" element={<EditSongPage />} /><Route path="/songs/:songId" element={<SongDetailPage />} />
         <Route path="/organizations" element={<OrganizationPage />} /><Route path="/organizations/:organizationId" element={<OrganizationDetailPage />} /><Route path="/services/:serviceId" element={<ServiceDetailPage />} /><Route path="/organizations/:organizationId/teams/:teamId" element={<TeamPage />} /><Route path="/bands" element={<BandListPage />} /><Route path="/bands/invite/:token" element={<BandInvitePage />} /><Route path="/bands/:bandId" element={<BandDetailPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
